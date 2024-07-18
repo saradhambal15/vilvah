e61c214645a2413ea153b79140a18c3384356621
manojrajendran <manojy2k13@gmail.com>
2024-07-19 01:06:33 +0530

    volvah

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+document.getElementById('shop-now').addEventListener('click', function() {
+    alert('Redirecting to shop!');
+});
+
+
+//document.getElementById('add-to-cart').addEventListener('click', function() {
+//alert('Added to cart!');
+//});
